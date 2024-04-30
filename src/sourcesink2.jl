9b80f099de672b1dc9c64acfdfded0398cb41fb1
Jonathan St-Onge <jonathanstonge7@gmail.com>
2024-04-30 10:11:27 -0400

    update sourcesink2

--- src/sourcesink2.jl
+++ src/sourcesink2.jl
@@ -1,10 +1,4 @@
-import Distributions
-import RecursiveArrayTools
-import OrdinaryDiffEq
-import ArgParse
-import SQLite
-import DataFrames
-
+using Distributions, OrdinaryDiffEq, RecursiveArrayTools, ArgParse
 include("helpers.jl")
 
 function parse_commandline()
@@ -65,73 +59,93 @@ function parse_commandline()
     return ArgParse.parse_args(s)
 end
 
-function initialize_u0(;n::Int=20, L::Int=6, M::Int=100, p::Float64=0.001,
-  lvl_1_inf::Bool=false)
+function initial_cond(;n::Int=20, L::Int=4, M::Int=1000000, p::Float64=0.0001, lvl_1::Bool=false)
   G = zeros(L, n+1)
 
-  if lvl_1_inf # 99% of population at the lowest level
+  if lvl_1 # 99% of population at the lowest level
     M /= 10
     for _ in 1:99*M
-      i = sum(collect(rand(Distributions.Binomial(1, p), n))) # how many total adopters?
+      i = sum(collect(rand(Binomial(1, p), n))) # how many total infectees?
       G[1, i+1] += 1 # everytime combination [1,i], count +1
     end
     for _ in 1:M
       ℓ = rand(2:L) # pick a level
-      i = sum(collect(rand(Distributions.Binomial(1, 0.01*p), n))) # how many total adopters?
+      i = sum(collect(rand(Binomial(1, p), n))) # how many total infectees?
       G[ℓ, i+1] += 1 # everytime combination [ℓ,i], count +1
     end
     G = G ./ (100*M) # normalized by tot number of groups
-    return RecursiveArrayTools.ArrayPartition(Tuple([G[ℓ,:] for ℓ=1:L]))
-  else 
+    return ArrayPartition(Tuple([G[ℓ,:] for ℓ=1:L])) 
+  else # uniform distribution of population across levels
       for _ in 1:M
         ℓ = rand(1:L) # pick a level
-        i = sum(collect(rand(Distributions.Binomial(1, p), n))) # how many total adopters?
+        i = sum(collect(rand(Binomial(1, p), n))) # how many total infectees?
         G[ℓ, i+1] += 1 # everytime combination [ℓ,i], count +1
       end
     G = G ./ M # normalized by tot number of groups
-    return RecursiveArrayTools.ArrayPartition(Tuple([G[ℓ,:] for ℓ=1:L]))
+    return ArrayPartition(Tuple([G[ℓ,:] for ℓ=1:L]))
   end
 end
 
-# function to switch between simple (ξ = 1) and complex contagion (ξ ≠ 1)
-g(x; ξ=1.) = x^ξ # for x ∈ ℕ ⇒ ξ = 1: linear growth; 0 < ξ < 1: sublinear growth; ξ > 1: superlinear growth
+g(x; ξ=1) = x^ξ # function to choose between linear (ξ = 1) and nonlinear contagion (ξ ≠ 1)
 
-function source_sink2!(du, u, p, t)
-    G, L, n = u, length(u.x), length(first(u.x))
-    β, ξ, α, γ, ρ, η, b, c, μ = p
-    Z, pop, R = zeros(L), zeros(L), 0.
-
-    # Calculate mean-field coupling and observed fitness landscape
-    for ℓ in 1:L
-        n_adopt = collect(0:(n-1))
-        Z[ℓ]    = sum(exp.(-b*n_adopt .- c*(ℓ-1)) .* G.x[ℓ]) 
-        pop[ℓ]  = sum(G.x[ℓ])
-        R      += sum(ρ * n_adopt .* G.x[ℓ]) 
-        pop[ℓ] > 0.0 && ( Z[ℓ] /= pop[ℓ] ) 
-      end
-      
-      for ℓ = 1:L, i = 1:n
-        n_adopt, gr_size = i-1, n-1
-        # Diffusion events
-        du.x[ℓ][i] = -γ*n_adopt*G.x[ℓ][i] - β*(ℓ^-α)*g(n_adopt+R, ξ=ξ)*(gr_size-n_adopt)*G.x[ℓ][i]
-        n_adopt > 0 && ( du.x[ℓ][i] += β*(ℓ^-α)*g(n_adopt-1+R, ξ=ξ)*(gr_size-n_adopt+1)*G.x[ℓ][i-1])
-        n_adopt < gr_size && ( du.x[ℓ][i] +=  γ*(n_adopt+1)*G.x[ℓ][i+1] )
-        # Group selection process
-        ℓ > 1 && ( du.x[ℓ][i] += η*G.x[ℓ-1][i]*(Z[ℓ] / Z[ℓ-1] + μ) - η*G.x[ℓ][i]*(Z[ℓ-1] / Z[ℓ] + μ) )
-        ℓ < L && ( du.x[ℓ][i] += η*G.x[ℓ+1][i]*(Z[ℓ] / Z[ℓ+1] + μ) - η*G.x[ℓ][i]*(Z[ℓ+1] / Z[ℓ] + μ) )
-      end
+@doc raw"""
+Key parts of the model:
+```
+# eq.2 and eq.3
+for ℓ in 1:L
+  n_infect = collect(0:(n-1))
+  R += sum(ρ * n_infect .* G.x[ℓ])
+  pop[ℓ] = sum(G.x[ℓ])
+  Z[ℓ] = pop[ℓ] > 0 ? sum(exp.(-b*n_infect .- c*(ℓ-1)) .* G.x[ℓ])/pop[ℓ] : 0. 
 end
 
-function run_source_sink2(p; perc_inf::Float64=0.001, lvl_1_inf::Bool=false)
-  n, M = 20, 10000
-  L = 4
-  u₀ = initialize_u0(n=n, L=L, M=M, p=perc_inf, lvl_1_inf=lvl_1_inf)
+# eq.1 and eq.4
+for ℓ = 1:L, i = 1:n
+  n_infect, gr_size = i-1, n-1
+  # Diffusion
+  du.x[ℓ][i] = -γ*n_infect*G.x[ℓ][i] - β*(ℓ^-α)*g(n_infect+R, ξ=ξ)*(gr_size-n_infect)*G.x[ℓ][i]
+  n_infect > 0 && ( du.x[ℓ][i] += β*(ℓ^-α)*g(n_infect-1+R, ξ=ξ)*(gr_size-n_infect+1)*G.x[ℓ][i-1])
+  n_infect < gr_size && ( du.x[ℓ][i] += γ*(n_infect+1)*G.x[ℓ][i+1] )
+  # Selection
+  ℓ > 1 && ( du.x[ℓ][i] += η*G.x[ℓ-1][i]*(Z[ℓ] / Z[ℓ-1] + μ) - η*G.x[ℓ][i]*(Z[ℓ-1] / Z[ℓ] + μ) )
+  ℓ < L && ( du.x[ℓ][i] += η*G.x[ℓ+1][i]*(Z[ℓ] / Z[ℓ+1] + μ) - η*G.x[ℓ][i]*(Z[ℓ+1] / Z[ℓ] + μ) )
+end
+```
 
-  tspan = (1.0, 10000)
+See the paper for the description.
+"""
+function dynamics!(du, u, p, t)
+  G, L, n = u, size(u.x,1), size(first(u.x),1)
+  β, ξ, α, γ, ρ, η, b, c, μ = p
+  Z, pop, R = zeros(L), zeros(L), 0.
+
+  # Mean-field coupling and fitness values
+  for ℓ in 1:L
+    n_infect = collect(0:(n-1))
+    R += sum(ρ * n_infect .* G.x[ℓ])
+    pop[ℓ] = sum(G.x[ℓ])
+    Z[ℓ] = pop[ℓ] > 0 ? sum(exp.(-b*n_infect .- c*(ℓ-1)) .* G.x[ℓ])/pop[ℓ] : 0. 
+  end
+    
+  for ℓ = 1:L, i = 1:n
+    n_infect, gr_size = i-1, n-1
+    # Diffusion
+    du.x[ℓ][i] = -γ*n_infect*G.x[ℓ][i] - β*(ℓ^-α)*g(n_infect+R, ξ=ξ)*(gr_size-n_infect)*G.x[ℓ][i]
+    n_infect > 0 && ( du.x[ℓ][i] += β*(ℓ^-α)*g(n_infect-1+R, ξ=ξ)*(gr_size-n_infect+1)*G.x[ℓ][i-1])
+    n_infect < gr_size && ( du.x[ℓ][i] += γ*(n_infect+1)*G.x[ℓ][i+1] )
+    # Selection
+    ℓ > 1 && ( du.x[ℓ][i] += η*G.x[ℓ-1][i]*(Z[ℓ] / Z[ℓ-1] + μ) - η*G.x[ℓ][i]*(Z[ℓ-1] / Z[ℓ] + μ) )
+    ℓ < L && ( du.x[ℓ][i] += η*G.x[ℓ+1][i]*(Z[ℓ] / Z[ℓ+1] + μ) - η*G.x[ℓ][i]*(Z[ℓ+1] / Z[ℓ] + μ) )
+  end
+end
+
+function run_source_sink2(p; L=L, t_max::Int=20000, perc_inf::Float64=p₀, lvl_1::Bool=false)
+  n, M = 20, 1000000
+  u₀ = initial_cond(n=n, L=L, M=M, p=perc_inf, lvl_1=lvl_1)
+  tspan = (1, t_max)
   
-  # Solve problem
-  prob = OrdinaryDiffEq.ODEProblem(source_sink2!, u₀, tspan, p)
-  return OrdinaryDiffEq.solve(prob, OrdinaryDiffEq.DP5(), saveat = 1., reltol=1e-8, abstol=1e-8)
+  prob = ODEProblem(dynamics!, u₀, tspan, p)
+  return solve(prob, Tsit5(), saveat = 1, reltol=1e-8, abstol=1e-8)
 end
 
 function main()
@@ -179,56 +193,3 @@ end
 if abspath(PROGRAM_FILE) == @__FILE__
   main()
 end
-
-# prototyping -------------------------------------------------------------------------------
-
-# using LaTeXStrings
-# using Plots
-
-# params_name = "β", "ξ", "α", "γ", "ρ", "η", "b", "c", "μ"
-
-# lvl_1_inf = false
-# perc_inf = 0.001
-# p = [0.1, 1., 1., 1., 0.1, 0.05, -1, 1., 0.0001]  # β, ξ, α, γ, ρ, η, b, c, μ
-# sol = run_source_sink2(p, perc_inf = perc_inf, lvl_1_inf=lvl_1_inf)
-# res, res_prop = parse_sol(sol)
-# t_max = 9999
-
-# function plot_value(res)
-#   L = length(res)
-#   xval = [res[l][1:t_max] for l=1:L]
-#   plot(xval, 
-#     xscale=:log, 
-#     # ylabel = L"\textrm{prevalence}", 
-#     labels = " " .* string.([1:L;]'),
-#     width = 3., 
-#     legendtitle = L"\textrm{level}",
-#     palette = palette(:Reds)[3:9], 
-#     legend=:left,
-#     xticks = 10 .^ [0,1,2,3,4]
-#   );
-  
-#   global_freq = [sum([res[ℓ][t]*res_prop[ℓ][t] for ℓ in 1:L]) for t in 1:t_max]
-  
-#   plot!(1:t_max, global_freq[1:t_max], width = 3,
-#         color =:black, ls =:dash, label = L"\textrm{global}",
-#         title = join([params_name[i] * "=" * string.(p)[i] for i in 1:length(params_name)], "  "))
-  
-# end
-
-# plot_value(res)
-
-# function plot_value_prop(res_prop)
-#   L = length(res)
-#   xval = [res_prop[l][1:t_max] for l=1:L]
-#   plot(xval, xscale=:log, ylabel = L"\textrm{level\ proportion}",
-#        labels = " " .* string.([1:L;]'),
-#        width = 3., 
-#        legendtitle = L"\textrm{level}", 
-#        palette = palette(:Blues)[3:9], 
-#        legend=:outerright,
-#        title = join([params_name[i] * "=" * string.(p)[i] for i in 1:length(params_name)], "  "),
-#        xticks = 10 .^ [0,1,2,3,4])
-# end
-
-# plot_value_prop(res_prop)
